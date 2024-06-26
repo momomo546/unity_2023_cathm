90a18dc65c8888db74b213b65fcdc95da0a50b28
momomo546 <108210454+momomo546@users.noreply.github.com>
2024-06-26 15:36:24 +0900

    score表示修正

--- Assets/Script/Score_script/Score_show.cs
+++ Assets/Script/Score_script/Score_show.cs
@@ -5,8 +5,13 @@ using TMPro;
 
 public class Score_show : MonoBehaviour
 {
-    [SerializeField]
-    private TextMeshProUGUI ScoreText;
+    [SerializeField]private TextMeshProUGUI ScoreText;
+    [SerializeField]private TextMeshProUGUI ComboText;
+    [SerializeField]private TextMeshProUGUI PerfectText;
+    [SerializeField]private TextMeshProUGUI GreatText;
+    [SerializeField]private TextMeshProUGUI GoodText;
+    [SerializeField]private TextMeshProUGUI BadText;
+    [SerializeField]private TextMeshProUGUI MissText;
 
     // Update is called once per frame
     void Start()
@@ -14,5 +19,23 @@ public class Score_show : MonoBehaviour
         Score.score = Score.Perfect*500+Score.Great*300+Score.Good*100+Score.Bad*10;
         ScoreText.SetText("Score:{0}\n",Score.score);
         Score.score=0;
+        
+        ComboText.SetText("Combo  :{0}\n",Score.Combo_max);
+        Score.Combo_max=0;
+        
+        PerfectText.SetText("Perfect:{0}\n",Score.Perfect);
+        Score.Perfect=0;
+        
+        GreatText.SetText("Great  :{0}\n",Score.Great);
+        Score.Great=0;
+
+        GoodText.SetText("Good   :{0}\n",Score.Good);
+        Score.Good=0;
+
+        BadText.SetText("Bad    :{0}\n",Score.Bad);
+        Score.Bad=0;
+
+        MissText.SetText("Miss   :{0}\n",Score.Miss);
+        Score.Miss=0;
     }
 }
